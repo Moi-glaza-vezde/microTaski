8d0e1c928f0d8ea053f49584d48de40ba5517f0f
Moi-glaza-vezde <moi.glaza.vezde@gmail.com>
2024-12-29 15:42:39 +0500

    useState done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Header } from './site/Header';
 import { Body } from './site/Body';
 import { Footer } from './site/Footer';
 
 function App() {
+   let [a, setA] = useState(1);
+   const onClickHandler = () => {
+      setA(++a);
+      console.log(a);
+   };
+
+   const onClickReset = () => {
+      setA(0);
+   };
    return (
       <>
+         <h2>{a}</h2>
+         <button onClick={onClickHandler}>number</button>
+         <button onClick={onClickReset}>0</button>
          <Header title={'NEW HEADER'} />
          <Body title={'NEW BODY'} />
          <Footer title={'NEW FOOTER'} />
