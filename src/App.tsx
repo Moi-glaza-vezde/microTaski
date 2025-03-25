900509c0cad10acce9631a3eb442bb4d18088d05
Moi-glaza-vezde <moi.glaza.vezde@gmail.com>
2025-03-25 08:43:54 +0500

    useSearceParams

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,61 @@
-import React from 'react';
-import {PageOne} from './components/pages/PageOne';
-import {PageTwo} from './components/pages/PageTwo';
-import {PageThree} from './components/pages/PageThree';
-import {Error404} from './components/pages/Error404';
+import React, {useState} from 'react';
+import styles from './components/Site.module.css';
+import {Adidas} from './components/pages/Adidas';
+import {Puma} from './components/pages/Puma';
+import {Abibas} from './components/pages/Abibas';
 import {Navigate, NavLink, Route, Routes} from 'react-router-dom';
+import {Error404} from './components/pages/Error404';
+import styled from 'styled-components';
 import {S} from './components/pages/_styles'
-
+import {Model} from './components/pages/Model';
+import {Prices} from './components/pages/Prices';
 
 
 const PATH = {
-    PAGE1: '/page1',
-    PAGE2: '/page2',
-    PAGE3: '/page3',
-    ERROR:'/error404'
-}as const
+    PAGE1: '/adidas',
+    PAGE2: '/puma',
+    PAGE3: '/abibas',
+    PAGE4: '/price'
 
+} as const
 
 function App() {
-
     return (
         <div>
-            <S.Header><h1>HEADER</h1></S.Header>
-            <S.Body>
-                <S.Nav>
+            <div className={styles.header}><h1>HEADER</h1></div>
+            <div className={styles.body}>
+                <div className={styles.nav}>
+                    <S.NavWrapper><NavLink to={PATH.PAGE1}>Adidas</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={PATH.PAGE2}>Puma</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={PATH.PAGE3}>Abibas</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={PATH.PAGE4}> Цена для оптовиков</NavLink></S.NavWrapper>
 
-                    {/*<div><NavLink className={({isActive}) => isActive ? s.activeNavLink : s.navLink}*/}
-                    {/*              to={'/page1'}>page1</NavLink></div>*/}
-                    {/*<div><NavLink className={({isActive}) => isActive ? s.activeNavLink : s.navLink}*/}
-                    {/*              to={'/page2'}>page2</NavLink></div>*/}
-                    {/*<div><NavLink className={({isActive}) => isActive ? s.activeNavLink : s.navLink}*/}
-                    {/*              to={'/page3'}>page3</NavLink></div>*/}
-
-
-                    <S.NavWrapper><NavLink to={PATH.PAGE1}>page1</NavLink></S.NavWrapper>
-                    <S.NavWrapper><NavLink to={PATH.PAGE2}>page2</NavLink></S.NavWrapper>
-                    <S.NavWrapper><NavLink to={PATH.PAGE3}>page3</NavLink></S.NavWrapper>
-
-                </S.Nav>
-                <S.Content>
+                </div>
+                <div className={styles.content}>
                     <Routes>
-                        <Route path="/" element={<Navigate to="/page1"/>}/>
-                        <Route path={PATH.PAGE1} element={<PageOne/>}/>
-                        <Route path={PATH.PAGE2} element={<PageTwo/>}/>
-                        <Route path={PATH.PAGE3} element={<PageThree/>}/>
-                        <Route path="*" element={<Error404/>}/>
+                        <Route path={'/'} element={<Navigate to={PATH.PAGE1}/>}/>
+
+                        <Route path={PATH.PAGE1} element={<Adidas/>}/>
+                        <Route path={PATH.PAGE2} element={<Puma/>}/>
+                        <Route path={PATH.PAGE3} element={<Abibas/>}/>
+                        <Route path={PATH.PAGE4} element={<Prices/>}/>
+                        {/*<Route path={'/adidas/:id'} element={<Model/>}/>*/}
+                        <Route path={'/:model/:id'} element={<Model/>}/>
+
+                        <Route path={'/*'} element={<Error404/>}/>
+
+                        {/*<Route path={'/page/error'} element={<Error404/>}/>*/}
+                        {/*<Route path={'/*'} element={<Navigate to={'/page/error'}/>}/>*/}
                     </Routes>
 
-                    {/*<Error404/>*/}
-                </S.Content>
-            </S.Body>
-            <S.Footer>abibas 2023</S.Footer>
+                </div>
+            </div>
+            <div className={styles.footer}>abibas 2023</div>
         </div>
     );
 }
 
-
 export default App;
+
+
+
--- src/components/pages/Error404.tsx
+++ src/components/pages/Error404.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import {useLocation} from 'react-router-dom';
 
 export const Error404 = () => {
-    const location = useLocation()
     return (
         <div>
-            <h1>Ошибка 404</h1>
-            <p>Страница <code>{location.pathname}</code> не найдена.</p>
+            Error404
         </div>
     );
 };
